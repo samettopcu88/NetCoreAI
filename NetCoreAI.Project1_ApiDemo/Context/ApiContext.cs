d1d53f7f1a64293c84a8c803012c83ef2619ae73
samettopcu88 <samet.topcu.88@icloud.com>
2025-06-13 21:44:44 +0300

    Add project files.

---
+++ NetCoreAI.Project1_ApiDemo/Context/ApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreAI.Project1_ApiDemo.Entities;
+
+namespace NetCoreAI.Project1_ApiDemo.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=SZYMEK; initial catalog=ApiAIDb; integrated security=true; trustservercertificate=true");
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
