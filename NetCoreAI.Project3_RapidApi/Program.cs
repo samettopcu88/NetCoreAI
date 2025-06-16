97ee6df52ba82826c7a65d62d8ca8d7dca9885a7
samettopcu88 <samet.topcu.88@icloud.com>
2025-06-16 22:59:16 +0300

    RapidApi operation practice

---
+++ NetCoreAI.Project3_RapidApi/Program.cs
@@ -0,0 +1,30 @@
+﻿using NetCoreAI.Project3_RapidApi.ViewModels;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+
+
+var client = new HttpClient();
+List<ApiSeriesViewModel> apiSeriesViewModels = new List<ApiSeriesViewModel>();
+var request = new HttpRequestMessage // HttpRequestMessage ismindeki sınıfa istekte bulunur
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+    Headers =
+    {
+        { "x-rapidapi-key", "d3711deaacmsh0c41d39277ef1e2p100512jsn73ac568f3574" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    apiSeriesViewModels = JsonConvert.DeserializeObject<List<ApiSeriesViewModel>>(body);
+    foreach(var series in apiSeriesViewModels)
+    {
+        Console.WriteLine(series.rank + "- " + series.title + " Film Puanı: " + series.rating + " Yapım Yılı: " + series.year);
+    }
+}
+
+Console.ReadLine();
