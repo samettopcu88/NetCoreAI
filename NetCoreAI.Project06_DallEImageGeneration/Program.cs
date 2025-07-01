8100c1376a65f3c9978689b250a4cffd6797ef5d
samettopcu88 <samet.topcu.88@icloud.com>
2025-07-01 19:21:39 +0300

    project 6 image creator ai done

---
+++ NetCoreAI.Project06_DallEImageGeneration/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        string apiKey = "--- api key ---";
+        Console.Write("Çizilmesini istediğiniz içerik (example prompts): ");
+        string prompt = Console.ReadLine();
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+            var requestBody = new
+            {
+                prompt = prompt,
+                n = 1,
+                size = "1024x1024"
+            };
+
+            string jsonBody = JsonConvert.SerializeObject(requestBody);
+            var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/images/generations", content);
+            string responseString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseString);
+        }
+    }
+}
