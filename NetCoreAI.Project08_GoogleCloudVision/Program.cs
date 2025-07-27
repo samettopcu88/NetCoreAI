d363e73ddfd24bf47ced29cd7618644cd307fdd7
samettopcu88 <samet.topcu.88@icloud.com>
2025-07-27 16:43:26 +0300

    google cloud vision ocr project is done

---
+++ NetCoreAI.Project08_GoogleCloudVision/Program.cs
@@ -0,0 +1,33 @@
+﻿using Google.Cloud.Vision.V1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Resim yolunu giriniz:");
+        string imagePath = Console.ReadLine();
+        Console.WriteLine();
+
+        string credentialPath = @"--- servis json dosyası gelecek ---";
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
+
+        try
+        {
+            var client = ImageAnnotatorClient.Create();
+            var image = Image.FromFile(imagePath);
+            var response = client.DetectText(image);
+            Console.WriteLine("Resimdeki Metin:");
+            Console.WriteLine();
+            foreach (var annotination in response)
+            {
+                if (!string.IsNullOrEmpty(annotination.Description))
+                {
+                    Console.WriteLine(annotination.Description);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Bir hata oluştu {ex.Message}");
+        }
+    }
+}
